707c8a3e3826b9fe05034b027ef64637b6cf7d7f
Thiago dos Santos Paixão <tpaxadev@gmail.com>
2014-12-26 23:12:07 -0200

    Stopped ar Handling file uploads

--- router.js
+++ router.js
@@ -1,13 +1,12 @@
 var url = require('url');
 
-var route = function(handle,path,response){
+exports.route = function(handle,path,response,postData){
   console.log("Routing the request for " + path)
   if (typeof handle[path] === 'function'){
-    handle[path](response);
+    handle[path](response,postData);
   }else{
     console.log("No Request handler for "+ path);
     response.writeHead(404,{"Content-Type":"text/plain"});
     response.end('404 Not Found');
   }
 }
-exports.route = route;
--- server.js
+++ server.js
@@ -2,20 +2,27 @@ var http = require('http');
 var url = require('url');
 var clc = require('cli-color');
 
-var start = function(route,handle){
+exports.start = function(route,handle){
   
   var onRequest = function(req,res){
     
+    var postData = "";
     var urlFull = url.parse(req.url,true);
     var pathname = urlFull.pathname;
     console.log("Request for \'" + pathname + "\' received");
     
-    route(handle,pathname,res)
+    req.setEncoding("utf8");
+    req.on('data',function(postDataChunk){
+      postData += postDataChunk;
+      console.log("\n\nChunk of POST data received: '"+ postDataChunk +"'.");
+    });
+    req.on('end',function(){
+      route(handle,pathname,res,postData);
+    });
 
   }
   
   http.createServer(onRequest).listen(8888);
-  console.log(clc.cyan.bold("Server started"))
+  console.log(clc.cyan.bold("Server started at port 8888"));
 
 }
-exports.start = start;
