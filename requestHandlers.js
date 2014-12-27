4c37825571dc46a8e427811df5de738e1cf73355
Thiago dos Santos Paixão <tpaxadev@gmail.com>
2014-12-26 23:12:07 -0200

    Stopped ar Handling file uploads

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,19 +1,32 @@
 var clc = require('cli-color');
 var exec = require('child_process').exec;
+var querystring = require('querystring');
 
 exports.start = function(response){
   console.log(clc.yellow('Request for "start" was called'));
 
-  exec("find /",{ timeout: 10000, maxBuffer: 20000*1024 }, function (error, stdout, stderr) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end(stdout);
-  });
+  var body = '<html>'+
+      '<head>'+
+      '<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />'+
+      '<title>NodeBegginer</title>'+
+      '</head>'+
+      '<body>'+
+      '<form action="/upload" method="post">'+
+      '<textarea name="text" rows="20" cols="60"></textarea>'+
+      '<input type="submit" value="Submit text" />'+
+      '</form>'+
+      '</body>'+
+      '</html>';
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.end(body);
 
 }
-exports.upload = function(response){
+exports.upload = function(response,postData){
 
   console.log(clc.yellow('Request for "upload" was called'));
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Upload");
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("You've sent: " + querystring.parse(postData).text,'utf8');
+  response.end();
 
 }
